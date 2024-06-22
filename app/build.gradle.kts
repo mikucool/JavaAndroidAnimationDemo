7639dba6c8b17edb3dcc4dad6786522472067f8e
mikucool <q1548701641@gmail.com>
2024-06-23 00:23:31 +0800

    update: libs:appcompat,material, glide

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,9 +45,10 @@ android {
 
 dependencies {
 
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.10.0")
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("com.github.bumptech.glide:glide:4.16.0")
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
